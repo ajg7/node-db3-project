3a0393312522aba4389cd176e8d2e35ec504cb64
ajg7 <ajgebara@gmail.com>
2020-10-07 16:54:54 -0500

    added dependencies and 2 SQL queries

--- queries.sql
+++ queries.sql
@@ -2,7 +2,11 @@
 
 -- Display the ProductName and CategoryName for all products in the database. Shows 77 records.
 
+SELECT product.ProductName, category.categoryName FROM product JOIN category ON product.SupplierId = category.id;
+
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
+
+SELECT [order].id, shipper.CompanyName FROM [order] JOIN shipper on [order].ShipVia = shipper.Id WHERE [order].OrderDate < '2012-08-09';
 
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
 
